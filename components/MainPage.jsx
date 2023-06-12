cfe1c3f45c6571cba3629c10431f9ed341cc165e
wallace-y <cameronwfinney@gmail.com>
2023-06-12 10:26:40 +0100

    adding stack dependecy and fixing stack routing

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,12 +1,10 @@
-
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default MainPage = () => {
-
-    return (
-        <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    )
-}
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
