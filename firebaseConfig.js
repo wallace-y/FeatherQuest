edb034c4d2ff19d3106d85c6597f8b939ff47add
waiKin <mou@waiKins-MacBook-Pro.local>
2023-06-13 09:58:02 +0100

    conflict

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  setDoc,
-  doc,
-  Firestore,
-} from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -28,22 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
-
-// async function getData(){
-//     const arr = [];
-//     await getDocs(collection(db, "birds"))
-//     .then((data) => {
-//     data.forEach(bird=>{
-//     console.log(bird._document.data.value.mapValue.fields);
-//     arr.push(bird._document.data.value.mapValue.fields);
-//     })
-// });
-// return arr;
-// }
-
-// console.log(getData())
