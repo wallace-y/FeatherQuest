d85a5f052579faf33a98cc12033842e4e9ddfe24
wallace-y <cameronwfinney@gmail.com>
2023-06-13 11:55:50 +0100

    adding navigation bar to the species page

--- components/Species.jsx
+++ components/Species.jsx
@@ -52,7 +52,7 @@ export default Species = ({ navigation }) => {
             <TouchableOpacity
               onPress={() => {
                 navigation.navigate("Bird", bird);
-                // console.log(bird);
+                navigation.navigate()
               }}
             >
               <Image
