11cfb6e8f012017617fc684004b91029bde490d1
waiKin <mou@waiKins-MacBook-Pro.local>
2023-06-13 10:01:51 +0100

    fixing conflict

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  doc,
+  Firestore,
+} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
