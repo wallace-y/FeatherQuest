866236463662bd9960047abdcd5ea29e36b93114
benvhshed <47852378+benvstars@users.noreply.github.com>
2023-06-14 09:52:48 +0100

    cleanup

--- utils/pullUserInfo.js
+++ utils/pullUserInfo.js
@@ -1,32 +1,5 @@
-import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+import { getDoc, doc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
-import { auth } from "../firebaseConfig";
-
-const user1 = auth.currentUser;
-
-
-// async function getUserData(id){
-
-//     const arr = [];
-//     await getDocs(collection(db, "users", id))
-//     .then((data) => {
-//         console.log('data in util', data);
-//     data.forEach(user=>{
-//     // console.log(user._document.data.value.mapValue.fields);
-//     arr.push(user._document.data.value.mapValue.fields);
-//     })
-// });
-// console.log(arr);
-// return arr;
-// }
-// async function getUserData(id){
-//     const userRef = collection(db, "users");
-//     return query(citiesRef, where("uid", "==", id));
-//     // const docRef = doc(db, 'users', id);
-//     // const docSnap = await getDoc(docRef);
-//     // // console.log(docSnap.data());
-//     // return docSnap.data()
-// }
 
 async function getUserData (id) {
     const userRef = doc(db, "users", id);
