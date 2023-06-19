bf40f36b1fbcc61ef7bd77ad7cce724008e77072
Rim0Rad <109389956+Rim0Rad@users.noreply.github.com>
2023-06-19 16:42:24 +0100

    Update Species.jsx
    searchQuerry was intialy set to false, that coused a warning. changed it to empty string

--- components/Species.jsx
+++ components/Species.jsx
@@ -22,7 +22,7 @@ export default Species = ({ navigation }) => {
   const [birds, setBirds] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [searchQuery, setSearchQuery] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredList, setFilteredList] = useState([]);
 
   useEffect(() => {
