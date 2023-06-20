a3955265b86c7f6eda12de5c232d39faf4d96ef1
benvhshed <47852378+benvstars@users.noreply.github.com>
2023-06-20 16:26:08 +0100

    added own location to maps by default

--- components/Maps/Maps.jsx
+++ components/Maps/Maps.jsx
@@ -1,61 +1,71 @@
-import MapView, {PROVIDER_GOOGLE } from 'react-native-maps'; //https://github.com/react-native-maps/react-native-maps/blob/master/docs/mapview.md
-import { View, Text, StyleSheet} from 'react-native';
-import { collection, getDocs} from "firebase/firestore";
-import { useEffect, useState} from 'react';
-import { db } from '../../firebaseConfig.js'
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps"; //https://github.com/react-native-maps/react-native-maps/blob/master/docs/mapview.md
+import { View, Text, StyleSheet } from "react-native";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState, useContext } from "react";
+import { db } from "../../firebaseConfig.js";
+import { UserContext } from "../../utils/UserContext.js";
 
-import SightingMarker from './SightingMarker.jsx';
-import mapStyle from '../../styles/mapStyle.js'
-import * as React from 'react'
+import SightingMarker from "./SightingMarker.jsx";
+import mapStyle from "../../styles/mapStyle.js";
+import * as React from "react";
 
 export default Maps = ({ navigation }) => {
-  
-  const [ sightingsList, setSightingList ] = useState([]);
-  const [ loadingMarkers, setLoadingMarkers ] = useState(true);
-  const [ mapCentered, setMapCentered] = useState(false);
+  const [sightingsList, setSightingList] = useState([]);
+  const [loadingMarkers, setLoadingMarkers] = useState(true);
+  const [mapCentered, setMapCentered] = useState(false);
+  const { globalUser } = useContext(UserContext);
 
   const region = {
-    latitude: 53.475906,
-    longitude: -2.248188,
+    latitude: Number(globalUser.coordinates[0]),
+    longitude: Number(globalUser.coordinates[1]),
     latitudeDelta: 0.01,
-    longitudeDelta: 0.01
+    longitudeDelta: 0.01,
   };
-  
-  
-  //Fetch sightings data
-  useEffect( () => {
-    getDocs(collection(db, "sightings"))
-    .then((data) => {
-        const sightingArr =  []
-        data.forEach( sighting => {
-          sightingArr.push( sighting.data())
-        })
-        setSightingList(sightingArr)
-        setLoadingMarkers(false)
-    })
-    .catch( err => {
-      console.log(" Failed to fetch sightings", err)
-    })
-  }, [])
 
-  if( loadingMarkers ){
-    return <Text>Loading...</Text>
-  }else{
+  //Fetch sightings data
+  useEffect(() => {
+    getDocs(collection(db, "sightings"))
+      .then((data) => {
+        const sightingArr = [];
+        data.forEach((sighting) => {
+          sightingArr.push(sighting.data());
+        });
+        setSightingList(sightingArr);
+        setLoadingMarkers(false);
+      })
+      .catch((err) => {
+        console.log(" Failed to fetch sightings", err);
+      });
+  }, []);
+  if (loadingMarkers) {
+    return <Text>Loading...</Text>;
+  } else {
     return (
       <View styles={styles.container}>
         <Text style={styles.headerText}>Sightings Near You</Text>
         <View style={styles.mapContainer}>
-       
-          <MapView 
+          <MapView
             provider={PROVIDER_GOOGLE}
-            onRegionChangeComplete={()=> { setMapCentered(true)} }
-            onRegionChange={()=> { setMapCentered(false)} }
+            onRegionChangeComplete={() => {
+              setMapCentered(true);
+            }}
+            onRegionChange={() => {
+              setMapCentered(false);
+            }}
             style={styles.map}
             initialRegion={region}
             customMapStyle={mapStyle}
-            >
-            {sightingsList.map( (sighting) => {
-              return <SightingMarker key={sighting.created_at} navigation={navigation} sighting={sighting} mapCentered={mapCentered} setMapCentered={setMapCentered}/>
+          >
+            {sightingsList.map((sighting) => {
+              return (
+                <SightingMarker
+                  key={sighting.created_at}
+                  navigation={navigation}
+                  sighting={sighting}
+                  mapCentered={mapCentered}
+                  setMapCentered={setMapCentered}
+                />
+              );
             })}
           </MapView>
         </View>
@@ -65,35 +75,33 @@ export default Maps = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: "#7A918D",
     borderWidth: 2,
-    color: 'red'
+    color: "red",
   },
-  headerContainer:{
+  headerContainer: {
     backgroundColor: "#7A918D",
     borderWidth: 2,
-    color: 'red'
+    color: "red",
   },
   headerText: {
-    minHeight: '5%',
-    textAlign: 'center',
+    minHeight: "5%",
+    textAlign: "center",
     fontSize: 20,
-    color: 'white',
-    backgroundColor: "#7A918D"
-
+    color: "white",
+    backgroundColor: "#7A918D",
   },
   mapContainer: {
-    borderRadius: 10, 
+    borderRadius: 10,
     borderWidth: 5,
-    borderColor: '#7A918D',
+    borderColor: "#7A918D",
     margin: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
     minHeight: "90%",
-    backgroundColor: "#7A918D"
-},
+    backgroundColor: "#7A918D",
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-  
-})
+});
--- components/postSightings/SelectLocation.jsx
+++ components/postSightings/SelectLocation.jsx
@@ -1,42 +1,51 @@
-import { View, Text, StyleSheet, Image} from 'react-native';
-import MapView, { Marker } from 'react-native-maps'; //https://github.com/react-native-maps/react-native-maps/blob/master/docs/mapview.md
-import { useState } from 'react';
-import { smallMapStyles, styles } from '../../styles/style.js';
-import mapStyle from '../../styles/mapStyle.js'
+import { View, Text, StyleSheet, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps"; //https://github.com/react-native-maps/react-native-maps/blob/master/docs/mapview.md
+import { useState } from "react";
+import { smallMapStyles, styles } from "../../styles/style.js";
+import mapStyle from "../../styles/mapStyle.js";
+import { useContext } from "react";
+import { UserContext } from "../../utils/UserContext.js";
 
-export default SelectLocation = ( { sightingData, setSightingData }) => {
+export default SelectLocation = ({ sightingData, setSightingData }) => {
+  const { globalUser } = useContext(UserContext);
 
-    const [ region, setRegion ] = useState({
-        latitude: 53.475906,
-        longitude: -2.248188,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-    })
+  const [region, setRegion] = useState({
+    latitude: Number(globalUser.coordinates[0]),
+    longitude: Number(globalUser.coordinates[1]),
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
 
-    const handleRegionChange = (region) => {
-        setRegion(region)
-        let tempSightingData = {...sightingData}
-        tempSightingData.coordinates = { coordinates: [region.longitude, region.latitude], type: "Point"}
-        setSightingData(tempSightingData)
-        
-    }
+  const handleRegionChange = (region) => {
+    setRegion(region);
+    let tempSightingData = { ...sightingData };
+    tempSightingData.coordinates = {
+      coordinates: [region.longitude, region.latitude],
+      type: "Point",
+    };
+    setSightingData(tempSightingData);
+  };
 
-    return (
-         <View style={smallMapStyles.container}>
-            <View>
-                <Text style={styles.textMedium}>Select Location</Text>
-            </View>
-            <View style={smallMapStyles.mapContainer}>
-                <MapView style={smallMapStyles.map}
-                    initialRegion={region}
-                    onRegionChangeComplete={handleRegionChange}
-                    customMapStyle={mapStyle}>
-                    <Marker coordinate={region} style={{height: 35, width:35}}>
-                        <Image source={require('../../assets/Binos2.png')} style={{height:25, width:35}}/>
-                    </Marker>
-                </MapView>
-            </View>
-        </View>
-    )
-}
-
+  return (
+    <View style={smallMapStyles.container}>
+      <View>
+        <Text style={styles.textMedium}>Select Location</Text>
+      </View>
+      <View style={smallMapStyles.mapContainer}>
+        <MapView
+          style={smallMapStyles.map}
+          initialRegion={region}
+          onRegionChangeComplete={handleRegionChange}
+          customMapStyle={mapStyle}
+        >
+          <Marker coordinate={region} style={{ height: 35, width: 35 }}>
+            <Image
+              source={require("../../assets/Binos2.png")}
+              style={{ height: 25, width: 35 }}
+            />
+          </Marker>
+        </MapView>
+      </View>
+    </View>
+  );
+};
