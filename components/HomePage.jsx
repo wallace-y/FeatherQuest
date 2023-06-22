e54f628bb2fd266cb6ce6fe2fc5997ebca5758ed
wallace-y <cameronwfinney@gmail.com>
2023-06-22 09:48:49 +0100

    removing navbar on homepage to force global user to update

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -24,6 +24,7 @@ export default HomePage = ({ navigation, route }) => {
     const fetchData = async () => {
       try {
         await getUserLocation().then((location) => {
+          console.log(location)
           setUserLocation([...location]);
           setLoadingUserLocation(false);
         });
